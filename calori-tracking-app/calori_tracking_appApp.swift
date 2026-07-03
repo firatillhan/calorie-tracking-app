b9cae6bbe97ae25d79017b8790888c4faeaae0e2
firatillhan <firatilhan008@gmail.com>
2026-07-03 22:34:50 +0300

    Initial Commit

---
+++ calori-tracking-app/calori_tracking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calori_tracking_appApp.swift
+//  calori-tracking-app
+//
+//  Created by Fırat İlhan on 3.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct calori_tracking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
